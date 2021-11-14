120969e9f00ec2c1aed673093c2b494c001217dd
pori777 <koichionepiece0729@icloud.com>
2021-11-15 07:38:45 +0900

    menuバーを動かすやつを設置

---
+++ script.js
@@ -0,0 +1,9 @@
+function menuListIcon() {
+    document.getElementById('line1').classList.toggle('line_1');
+    document.getElementById('line2').classList.toggle('line_2');
+    document.getElementById('line3').classList.toggle('line_3');
+    document.getElementById('menuList').classList.toggle('in');
+    }
+    document.getElementById('menuListIcon').addEventListener('click' , function () {
+    menuListIcon();
+    } );
