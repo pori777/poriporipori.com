7083ef8766a9bca94fe35842446ec85adba9ac11
pori777 <koichionepiece0729@icloud.com>
2021-11-26 14:30:00 +0900

    index.htmlのフッター部分を移した

---
+++ footer.php
@@ -0,0 +1,11 @@
+</div>
+
+            <footer>
+                <div class="copyright">
+                    <h5>© 2021 pori</h5>
+                </div>
+            </footer>
+        </div>
+        <script src="script.js"></script>
+    </body>
+</html>
