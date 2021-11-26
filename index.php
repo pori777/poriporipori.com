36a8b67d2f3b44012b203104f5f41b5bbc35921f
pori777 <koichionepiece0729@icloud.com>
2021-11-27 08:02:52 +0900

    開始

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale1.0">
+        <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="script.js">
+    </head>
+
+    <body>
+        <div class="bg">
+            <header>
+                <div class="headerTitle">
+                    <h1>pori</h1>
+                </div>
+                <nav id="menuList">
+                    <ul>
+                        <li><a href="index.html">HOME</a></li>
+                        <li><a href="about.html">ABOUT</a></li>
+                        <li><a href="product.html">PRODUCT</a></li>
+                        <li><a href="contact.html">CONTACT</a></li>
+                    </ul>
+                </nav>
+
+                <div id="menuListIcon">
+                    <span class="innerLine" id="line1"></span>
+                    <span class="innerLine" id="line2"></span>
+                    <span class="innerLine" id="line3"></span>
+                </div>
+            </header>
+
+            <div id="HOMEspace">
+
+            </div>
+
+            <div id="PRODUCTspace">
+
+            </div>
+            
+            <div id="CONTACTspace">
+
+            </div>
+
+            <footer>
+                <div class="copyright">
+                    <h5>© 2021 pori</h5>
+                </div>
+            </footer>
+        </div>
+        <script src="script.js"></script>
+    </body>
+</html>
