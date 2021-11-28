d999a5d528830f3f6394b778d38bb5a98de0661b
pori777 <koichionepiece0729@icloud.com>
2021-11-28 10:29:38 +0900

    ファイル名をfrontpage.php→front-page.phpに変更

---
+++ front-page.php
@@ -0,0 +1,11 @@
+<?php get_header() ;?>
+</div>
+
+            <div id="PRODUCTspace">
+
+            </div>
+            
+            <div id="CONTACTspace">
+                
+
+<?php get_footer() ;?>
