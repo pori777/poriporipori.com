a2ba8fce49c4cd3c904af951afff7fab7abec57c
pori777 <koichionepiece0729@icloud.com>
2021-11-28 10:16:15 +0900

    リセットcssのテンプレートurlの削除

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <title></title>
         <meta name="viewport" content="width=device-width, initial-scale1.0">
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>https://unpkg.com/ress/dist/ress.min.css">
+        <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
         <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/style.css">
         <script>src="<?php get_stylesheet_directory_uri();?>/js/script.js"</script>
     
