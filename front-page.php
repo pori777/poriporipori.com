302116402b5ebd723a4d8a456636969eb319f2a2
pori777 <koichionepiece0729@icloud.com>
2021-11-29 14:06:14 +0900

    お問い合わせを消去

--- front-page.php
+++ front-page.php
@@ -23,20 +23,4 @@
             <div class="btn_stn"><a href="<?php home_url('') ;?>/news">一覧をみる</a></div>
         </div>
 
-        
-    <p>お問い合わせはこちらのフォームをご利用ください。</p>
-    [contact-form-7 id="6" title="お問い合わせ"]
-    <label> 氏名
-    [text* your-name] </label>
-
-<label> メールアドレス
-    [email* your-email] </label>
-
-<label> メッセージ本文 
-    [textarea your-message] </label>
-
-[submit "送信"]
-
-    </section>
-
 <?php get_footer() ;?>
