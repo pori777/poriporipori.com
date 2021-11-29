e351083f822b6c0593bcd92ca5079bf6da350d94
pori777 <koichionepiece0729@icloud.com>
2021-11-29 16:09:24 +0900

    プラグインのコピーを消去してこれからやることをメモ書きした

--- contact-page.php
+++ contact-page.php
@@ -1,15 +1,9 @@
 <?php get_header(); ?>
-[contact-form-7 id="6" title="お問い合わせ"]
-<p>お問い合わせはこちらのフォームをご利用ください。</p>
-<label> 氏名
-    [text* your-name] </label>
-
-<label> メールアドレス
-    [email* your-email] </label>
-
-<label> メッセージ本文 
-    [textarea your-message] </label>
-
-[submit "送信"]
+<!-- お問い合わせフォームの作成
+    namae
+    meado
+    dennwabanngou
+    soudannnaiyou
+-->
 
 <?php get_footer(); ?>
