771912c00a8f43e7fe3a92492f11846482db1456
pori777 <koichionepiece0729@icloud.com>
2021-11-29 16:11:14 +0900

    三つのストロングポイントを書く欄のスペースを作成

--- front-page.php
+++ front-page.php
@@ -1,26 +1,5 @@
 <?php get_header() ;?>
 </div>
-
-            <div id="PRODUCTspace">
-
-            </div>
-            
-            <div id="CONTACTspace">
-    
-            <section class="front_news">
-        <div class="sec_head">お知らせ</div>
-        <div class="news_list">
-            <ul>
-                <?php if (have_posts()) : while (have_posts()) : the_post(); ?><!--  ループ開始  -->
-                <li>
-                    <a href="<?php the_permalink() ;?>">
-                        <div class="date"><?php echo get_the_date('Y.m.d') ;?></div>
-                        <div class="ttl"><?php the_title() ;?></div>
-                    </a>
-                </li>
-                <?php endwhile; endif; ?><!--  /ループ終わり -->
-            </ul>
-            <div class="btn_stn"><a href="<?php home_url('') ;?>/news">一覧をみる</a></div>
-        </div>
-
+<!--  三つの丸を作る-->
+            <div id="headerStrongPoint">
 <?php get_footer() ;?>
