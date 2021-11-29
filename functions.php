3ad45b3f9f0bec8101289be5d82cac379df6c83d
pori777 <koichionepiece0729@icloud.com>
2021-11-29 14:05:54 +0900

    機能の追加

--- functions.php
+++ functions.php
@@ -11,3 +11,13 @@ function link_scripts() {
 wp_enqueue_script( 'script.js', get_stylesheet_directory_uri() . '/js/script.js', '', '', true );
 }
 add_action('wp_enqueue_scripts', 'link_scripts' );
+
+function post_has_archive( $args, $post_type) {//設定後に（パーマリンク更新すること）
+    if ('post' == $post_type) {
+        $args['rewrite'] = true;
+        $args['has_archive'] = 'news';
+        $args['label'] = 'お知らせ';
+    }
+    return $args;
+}
+add_filter('register_post_type_args', 'post_has_archive', 10,2);
