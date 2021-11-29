6f6fbbb2dc9bcba661b3ef794200b322eb1f64bb
pori777 <koichionepiece0729@icloud.com>
2021-11-29 12:38:10 +0900

    お問い合わせフォームの追加

--- front-page.php
+++ front-page.php
@@ -22,6 +22,19 @@
             </ul>
             <div class="btn_stn"><a href="<?php bloginfo('url') ;?>/news">一覧をみる</a></div>
         </div>
+
+        [contact-form-7 id="6" title="お問い合わせ"]
+<p>お問い合わせはこちらのフォームをご利用ください。</p>
+<label> 氏名
+    [text* your-name] </label>
+
+<label> メールアドレス
+    [email* your-email] </label>
+
+<label> メッセージ本文 
+    [textarea your-message] </label>
+
+[submit "送信"]
     </section>
 
 <?php get_footer() ;?>
