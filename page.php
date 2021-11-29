55fadabd32315733201ddcca22bdc4510cb7f6ea
pori777 <koichionepiece0729@icloud.com>
2021-11-29 13:33:29 +0900

    説明文の追加

--- page.php
+++ page.php
@@ -2,9 +2,9 @@
 
 <div id="content" class="page_wrap W_inner">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?><!-- ループ開始 -->
-    <h1><?php the_title(); ?></h1>
+    <h1><?php the_title(); ?></h1><!-- リクエストしたページのタイトルを表示 -->
     <div class="page_container">
-        <php the_content(); ?>
+        <?php the_content(); ?><!--固定ページの管理画面の中身を表示 -->
     </div>
     <?php endwhile; endif; ?><!-- ループ終了 -->
 </div>
