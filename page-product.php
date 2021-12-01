8fbd7bfa38d7ee14e972cc96a3ca88b6fb000967
pori777 <koichionepiece0729@icloud.com>
2021-12-01 13:42:27 +0900

    get_header,footerの追加 ssectionタグを追加し、制作実績の大枠を作成

--- page-product.php
+++ page-product.php
@@ -1,2 +1,16 @@
 <!--カスタム投稿タイプの作成
 大まかな配置を考える-->
+<?php get_header(); ?>
+
+<section>
+    <h1>制作実績</h1>
+    <div class="container">
+        
+    </div>
+</section>
+
+
+
+
+
+<?php get_footer(); ?>
