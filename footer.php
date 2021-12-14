8fdea9c4a2c1b2d32d34b44ea0fc9e9232db3ddf
pori777 <koichionepiece0729@icloud.com>
2021-12-15 07:34:40 +0900

    完璧

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 </div>
-
-            <footer>
-                <div class="copyright">
-                    <h5>© 2021 pori</h5>
-                </div>
-            </footer>
-        <script src="<?php get_stylesheet_directory_uri();?>/js/script.js"></script>
-        <?php wp_footer(); ?>
+        <footer>
+            <div class="copyright">
+                <h5>© 2021 pori</h5>
+            </div>
+        </footer>
+</div>
+    <script src="<?php get_stylesheet_directory_uri();?>/js/script.js"></script>
+    <?php wp_footer(); ?>
     </body>
 </html>
