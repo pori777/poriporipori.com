eb0486a39268ba78561e86e3af64d5d37ce311e9
pori777 <koichionepiece0729@icloud.com>
2021-12-15 08:43:47 +0900

    section部分を廃止してdivにした

--- page-contact.php
+++ page-contact.php
@@ -1,7 +1,8 @@
 <?php get_header(); ?>
-</div>
-    <section>
-        <h1>お問い合わせは下記のフォームからお願いします!</h1>
-        <?php echo do_shortcode('[contact-form-7 id="38" title="無題"]'); ?>
-    </section>
+    <div class="contactSpace">
+    <h1>お問い合わせは下記のフォームからお願いします!</h1>
+        <div class="contactForm">
+            <?php echo do_shortcode('[contact-form-7 id="38" title="無題"]'); ?>
+        </div>
+    </div>
 <?php get_footer(); ?>
