f5e4a50882274a7eab01885afac679c2f6c885e1
pori777 <koichionepiece0729@icloud.com>
2021-12-15 09:47:50 +0900

    ショートコードの変更

--- page-contact.php
+++ page-contact.php
@@ -2,7 +2,7 @@
     <div class="contactSpace">
     <h1>お問い合わせは下記のフォームからお願いします!</h1>
         <div class="contactForm">
-            <?php echo do_shortcode('[contact-form-7 id="38" title="無題"]'); ?>
+            <?php echo do_shortcode('[contact-form-7 id="44" title="コンタクトフォーム"]'); ?>
         </div>
     </div>
 <?php get_footer(); ?>
